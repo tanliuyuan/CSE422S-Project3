4f8abbb544403708c40ed05e2bc895cb2ff0ab32
dlatuli1 <dlatuli1@yahoo.com>
2015-11-30 21:34:19 -0600

    Update vmgenWS2.h

--- vmgenWS2.h
+++ vmgenWS2.h
@@ -6,24 +6,25 @@
 #include <stdlib.h>
 #include <fstream>
 #include <vector>
+#include <time.h>
 using namespace std;
 
 class vmgenWS
 {
 public:
-
+    //constructor
 	vmgenWS(int size, int lb, int ub, int range, int length, string file) : ws_size(size), LB(lb), UB(ub), seqRange(range), seqLength(length), genFile(file) {}
+	//file generator
 	int generateFile();
 
 private:
 
 
-	int ws_size, LB, UB, seqLength, seqRange;
+	int ws_size, LB, UB, seqLength, seqRange; //command line inputs
 	int referencesCreated, num_gen, ref = 0;
-	vector<int> ws;
-	bool reroll = false;
+	vector<int> ws; //working set array
+	bool reroll = false; //used when duplicate values created within set
 	//default_random_engine intGenerate;
-	random_device rand1, rand2, rand3;
 	string genFile;
 	ofstream outFile;
 };
@@ -35,6 +36,6 @@ inline bool isInt(const std::string & s)
 
 	char * p;
 	strtol(s.c_str(), &p, 10);
-
+    // if strtol returns value of zero to p, no non-numerical values were found. return true
 	return (*p == 0);
 }
